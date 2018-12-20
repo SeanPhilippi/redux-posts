1663a300ed86e42e616b6bf2484991728ef35d73
SeanPhilippi <sean.philippi@gmail.com>
2018-12-20 15:10:06 -0600

    redux boilerplate

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
+// second argument is inital state, applyMiddleware() is an enhancer
+// const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
 
   render() {
 
     return (
-      <div className="App">
-        <Posts />
-        <hr />
-        <PostForm />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
